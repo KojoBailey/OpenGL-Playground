540570c635579b14410b2119fb8846f53278480a
Kojo Bailey <kojomolojo@gmail.com>
2026-02-09 23:25:56 +0000

    Wrap shader and program error checking in functions.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void test_shader(unsigned int shader, std::string_view name);
+void test_program(unsigned int program);
 
 const unsigned int SCREEN_WIDTH = 800;
 const unsigned int SCREEN_HEIGHT = 600;
@@ -46,15 +48,7 @@ int main()
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
-	{
-		int success = false;
-		char info_log_buffer[512];
-		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-		if (!success) {
-			glGetShaderInfoLog(vertexShader, 512, NULL, info_log_buffer);
-			std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << info_log_buffer << std::endl;
-		}
-	}
+	test_shader(vertexShader, "VERTEX");
 
 	const char* fragmentShaderSource = "#version 330 core\n"
 		"out vec4 FragColor;\n"
@@ -65,29 +59,13 @@ int main()
 	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
-	{
-		int success = false;
-		char info_log_buffer[512];
-		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-		if (!success) {
-			glGetShaderInfoLog(fragmentShader, 512, NULL, info_log_buffer);
-			std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << info_log_buffer << std::endl;
-		}
-	}
+	test_shader(fragmentShader, "FRAGMENT");
 
 	unsigned int shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
-	{
-		int success = false;
-		char info_log_buffer[512];
-		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-		if (!success) {
-			glGetProgramInfoLog(shaderProgram, 512, NULL, info_log_buffer);
-			std::cerr << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << info_log_buffer << std::endl;
-		}
-	}
+	test_program(shaderProgram);
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
@@ -159,3 +137,24 @@ void processInput(GLFWwindow* window)
 	}
 }
 
+void test_shader(unsigned int shader, std::string_view name)
+{
+	int success = false;
+	char info_log_buffer[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(shader, 512, NULL, info_log_buffer);
+		std::cerr << "ERROR::SHADER::" << name << "::COMPILATION_FAILED\n" << info_log_buffer << std::endl;
+	}
+}
+
+void test_program(unsigned int program)
+{
+	int success = false;
+	char info_log_buffer[512];
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(program, 512, NULL, info_log_buffer);
+		std::cerr << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << info_log_buffer << std::endl;
+	}
+}
